5f9e0decd96650958081a0f1565ec90323ea884f
ilaria04 <ilaria04@stanford.edu>
2024-11-18 22:31:36 -0800

    supabase

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SearchScreen from "./src/screens/SearchScreen";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
+import { supabase } from "./src/services/supabaseClient";
 
 const Stack = createNativeStackNavigator();
 
