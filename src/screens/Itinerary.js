093ad04649b68289bad929503f19ead150809650
kenbuii <work.kennethbui@gmail.com>
2024-11-18 20:35:02 -0800

    created itinerary screen
    todo: add functionality

---
+++ src/screens/Itinerary.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    TextInput,
+    FlatList,
+    Image,
+    Dimensions,
+    TouchableOpacity,
+  } from "react-native";
+
+import * as icons from "../assets/icons";
+import themes from "../assets/themes";
+
+
+
+
+const Itinerary = () => {
+  //TODO: add itinerary screen
+  return <View></View>;
+};
+
+export default Itinerary;
