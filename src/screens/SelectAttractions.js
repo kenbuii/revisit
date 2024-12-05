386f443e63bd1fde7eb5f119f5ee7b28247ea263
ilaria04 <ilaria04@stanford.edu>
2024-12-04 23:16:06 -0800

    font size change

--- src/screens/SelectAttractions.js
+++ src/screens/SelectAttractions.js
@@ -78,7 +78,7 @@ const SelectAttractions = () => {
     //     if (error) throw error;
 
     //     // Navigate to ChooseDate screen
-        navigation.navigate("ChooseDate");
+    navigation.navigate("ChooseDate");
     //   }
     // } catch (error) {
     //   console.error("Error saving activities:", error.message);
@@ -101,7 +101,7 @@ const SelectAttractions = () => {
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContent}>
         <View>
-          <Text style={styles.heading}>select your attractions</Text> 
+          <Text style={styles.heading}>select your attractions</Text>
         </View>
         <View style={styles.buttonContainer}>
           {activities.map((activity, index) => (
@@ -206,7 +206,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
   heading: {
-    fontSize: 24,
+    fontSize: 20,
     fontFamily: "RobotoMono-Bold",
     color: "black",
     textAlign: "center",
